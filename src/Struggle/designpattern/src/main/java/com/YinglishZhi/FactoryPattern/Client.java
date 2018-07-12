432f00059c60dbc4d8b526b8c271a4b18b3d2371
YinglishZhi <13703640931@163.com>
2018-07-12 23:17:58 +0800

    前几天的所有：大数据 + 设计模式

--- src/Struggle/designpattern/src/main/java/com/YinglishZhi/FactoryPattern/Client.java
+++ src/Struggle/designpattern/src/main/java/com/YinglishZhi/FactoryPattern/Client.java
@@ -4,14 +4,61 @@ import com.YinglishZhi.FactoryPattern.Factory.Creator;
 import com.YinglishZhi.FactoryPattern.Product.Product;
 import com.YinglishZhi.FactoryPattern.Product.impl.ConcreteProduct1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Client {
     public static void main(String[] args) {
         Product p = Creator.getProduct(ConcreteProduct1.class.getName());
         p.func();
 
+        Integer[] nums = new Integer[10];
 
-        String s = "1111";
+        List<List<Integer[]>>  re = new ArrayList<List<Integer[]>>();
+        List<Integer> te = new ArrayList<Integer>();
+        List<Integer> numsList = Arrays.asList(nums);
 
-        s.replace(0,3,s);
+        re.add((List<Integer[]>) numsList);
+
+        String[] string=new String[]{"s1","s2","s3","s4","s5"};
+
+        List<String> li= Arrays.asList(string);
+
+        System.out.println(li);
+    }
+
+
+
+
+
+    private List<List<Integer[]>> result = new ArrayList<List<Integer[]>>();
+
+    public List<List<Integer[]>> permute(int[] nums) {
+        // write your code here
+        return this.permute(nums, 0);
+    }
+
+    public List<List<Integer[]>> permute(int[] nums, int start) {
+        // write your code here
+
+        if(start == nums.length){
+            List<Integer[]> tem = new ArrayList<Integer[]>();
+            tem.add(nums);
+             result.add(tem);
+        }else{
+            for (int i = start; i < nums.length;++i){
+                swap(nums, i, start);
+                permute(nums, start + 1);
+                swap(nums, i, start);
+            }
+        }
+        return result;
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
