7a18321483313e6853a3edd58e11aa797a533418
刘德志 <dezhi.liu@yi23.net>
2019-07-06 20:34:47 +0800

    graph

--- src/Struggle/Algorithm/src/main/java/graph/src/Graph.java
+++ src/Struggle/Algorithm/src/main/java/graph/src/Graph.java
@@ -12,19 +12,6 @@ import java.util.LinkedList;
 @Slf4j
 public class Graph implements IGraph {
 
-    private static final char[] VERTEX = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'};
-    private static final char[][] EDGE = new char[][]{
-            {'A', 'C'},
-            {'A', 'D'},
-            {'A', 'F'},
-            {'B', 'C'},
-            {'C', 'D'},
-            {'E', 'G'},
-            {'D', 'G'},
-            {'I', 'J'},
-            {'J', 'G'},};
-
-
     public void depthFirstSearch(GraphMatrix graphMatrix) {
         int i;
         for (i = 0; i < graphMatrix.size; i++) {
@@ -66,16 +53,5 @@ public class Graph implements IGraph {
                 }
             }
         }
-    }
-
-    public static void main(String[] args) {
-        IGraph graph = new Graph();
-        GraphMatrix graphMatrix = new GraphMatrix(VERTEX, EDGE, true);
-        log.info("BFS");
-        graph.breadthFirstSearch(graphMatrix);
-        System.out.println();
-        log.info("DFS");
-        graphMatrix = new GraphMatrix(VERTEX, EDGE, true);
-        graph.depthFirstSearch(graphMatrix);
     }
 }
--- src/Struggle/Algorithm/src/main/java/graph/src/Solution.java
+++ src/Struggle/Algorithm/src/main/java/graph/src/Solution.java
@@ -1,10 +1,38 @@
 package graph.src;
 
+import graph.src.api.IGraph;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author LDZ
  * @date 2019-06-20 21:15
  */
+@Slf4j
 public class Solution {
+    /**
+     * vertex 边
+     */
+    private static final char[] VERTEX = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'};
+    private static final char[][] EDGE = new char[][]{
+            {'A', 'C'},
+            {'A', 'D'},
+            {'A', 'F'},
+            {'B', 'C'},
+            {'C', 'D'},
+            {'E', 'G'},
+            {'D', 'G'},
+            {'I', 'J'},
+            {'J', 'G'},};
 
+    public static void main(String[] args) {
+        IGraph graph = new Graph();
 
+        GraphMatrix graphMatrix = new GraphMatrix(VERTEX, EDGE, true);
+        log.info("BFS");
+        graph.breadthFirstSearch(graphMatrix);
+        System.out.println();
+        log.info("DFS");
+        graphMatrix = new GraphMatrix(VERTEX, EDGE, true);
+        graph.depthFirstSearch(graphMatrix);
+    }
 }
