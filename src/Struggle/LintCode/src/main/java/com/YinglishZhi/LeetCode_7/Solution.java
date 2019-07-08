3a9c18f17fa5d756aaa0ff1744b7cd324352aac8
刘德志 <dezhi.liu@yi23.net>
2019-07-09 00:27:31 +0800

    leetcode

---
+++ src/Struggle/LintCode/src/main/java/com/YinglishZhi/LeetCode_7/Solution.java
@@ -0,0 +1,22 @@
+package com.YinglishZhi.LeetCode_7;
+
+public class Solution {
+
+    private static int reverse(int x) {
+        int result = 0;
+        if (x == 0) {
+            return 0;
+        }
+        while (x != 0) {
+            result = result * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(reverse(1534236469));
+    }
+}
