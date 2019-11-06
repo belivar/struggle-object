dda0e7fbbf9c5fae3bc4208061b11ab4db736281
LDZ <13703640931@163.com>
2019-11-06 16:12:09 +0800

    kaka study

--- src/Struggle/leetcode/src/main/java/com/YinglishZhi/LeetCode_10/Solution.java
+++ src/Struggle/leetcode/src/main/java/com/YinglishZhi/LeetCode_10/Solution.java
@@ -16,37 +16,42 @@ public class Solution {
     private static final char STAR = '*';
 
     public static boolean isMatch(String s, String p) {
+        if (null == p || null == s) {
+            return false;
+        }
 
-        char[] sChar = s.toCharArray();
-        int j = 0;
-        for (int i = 0; i < sChar.length; i++) {
-            if (j < p.length()) {
-                char c = p.charAt(j);
-                if (sChar[i] == c) {
-                    j++;
-                } else {
-                    if (c == POINT) {
-                        j++;
-                        continue;
-                    }
-                    if (c == STAR) {
-                        char cPre = p.charAt(j - 1);
-                        if (cPre == sChar[i] || cPre == POINT) {
-                            continue;
-                        }
-                    }
-                    return false;
-                }
-            } else {
-                return false;
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == STAR && dp[0][i - 1]) {
+                dp[0][i + 1] = true;
             }
         }
-        return true;
+
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == POINT || p.charAt(j) == s.charAt(i)) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == STAR) {
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != POINT) {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+
     }
 
     // ====== test case ====
 
     private static final String AA = "aa";
+    private static final String AB = "ab";
 
     private static final String PATTERN_A = "a";
 
@@ -54,6 +59,8 @@ public class Solution {
 
     private static final String PATTERN_A_POINT = "a.";
     private static final String PATTERN_A_STAR = "a*";
+
+    private static final String PATTERN_POINT_STAR = ".*";
 
     @Test
     public void testCase1() {
@@ -73,5 +80,10 @@ public class Solution {
     @Test
     public void testCase4() {
         Assert.assertTrue(isMatch(AA, PATTERN_A_STAR));
+    }
+
+    @Test
+    public void testCase5() {
+        Assert.assertTrue(isMatch(AB, PATTERN_POINT_STAR));
     }
 }
